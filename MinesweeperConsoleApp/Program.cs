a3e3e9f565a0d9d60fe6d84403ef227f7675bca3
Etendo <etendo123@gmail.com>
2026-03-08 20:28:52 -0600

    Commented the Program script

--- MinesweeperConsoleApp/Program.cs
+++ MinesweeperConsoleApp/Program.cs
@@ -12,37 +12,47 @@ using MinesweeperClassLibrary.Services.BusinessLogicLayer;
 namespace MinesweeperConsoleApp
 {
     internal class Program
-    {
+    {   
         public static bool Start;
 
         static void Main(string[] args)
         {
-            Start = true;
+            // Start the game
+            Console.WriteLine("Hello, welcome to Minesweeper!");
 
-            while (Start)
-            {
-                Console.WriteLine("Hello, welcome to Minesweeper!");
+            // Display the answer keys for the first board
+            Console.WriteLine("Here is the answer key for the first board");
+            // Create a new board with difficulty level 1
+            BoardModel board = new BoardModel(1);
+            // Create a new instance of the MinesweeperLogic class
+            MinesweeperLogic minesweeperLogic = new MinesweeperLogic();
+            // Get the size of the board
+            minesweeperLogic.GetSize(board.Size);
+            // Set up the bombs on the board
+            minesweeperLogic.SetupBombs(board.Cells);
+            // Count the bombs on the board
+            minesweeperLogic.CountBombs();
+            // Print the answer key for the board
+            minesweeperLogic.PrintAnswers();
 
-                Console.WriteLine("Here is the answer key for the first board");
-                BoardModel board = new BoardModel(1);
-                MinesweeperLogic minesweeperLogic = new MinesweeperLogic();
-                minesweeperLogic.GetSize(board.Size);
-                minesweeperLogic.SetupBombs(board.Cells);
-                minesweeperLogic.CountBombs();
-                minesweeperLogic.PrintAnswers();
+            // Display the answer keys for the second board
+            Console.WriteLine("Here is the answer key for the second board");
+            // Create a new board with difficulty level 2
+            BoardModel board2 = new BoardModel(2);
+            // Create a new instance of the MinesweeperLogic class
+            MinesweeperLogic minesweeperLogic2 = new MinesweeperLogic();
+            // Get the size of the board
+            minesweeperLogic2.GetSize(board2.Size);
+            // Set up the bombs on the board
+            minesweeperLogic2.SetupBombs(board2.Cells);
+            // Count the bombs on the board
+            minesweeperLogic2.CountBombs();
+            // Print the answer key for the board
+            minesweeperLogic2.PrintAnswers();
 
-                Console.WriteLine("Here is the answer key for the second board");
-                BoardModel board2 = new BoardModel(2);
-                MinesweeperLogic minesweeperLogic2 = new MinesweeperLogic();
-                minesweeperLogic2.GetSize(board2.Size);
-                minesweeperLogic2.SetupBombs(board2.Cells);
-                minesweeperLogic2.CountBombs();
-                minesweeperLogic2.PrintAnswers();
-
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-                Start = false;
-            }
+            // Prompt the user to press any key to exit
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
